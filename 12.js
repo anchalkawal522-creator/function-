0189c672ea3e767962493e577a76e307170ac4ce
Anchal Katwal <anchalkatwal522@gmail.com>
2026-02-12 22:56:21 +0530

    12

---
+++ 12.js
@@ -0,0 +1,69 @@
+// let p1= new Promise((resolve,reject)=>{
+//   resolve(50)
+// })
+// p1.then(marks){
+//   if(marks>=90){
+//     console.log("grade a")
+//   }
+//   else if(marks>=60){
+//     console.log("grade b")
+//   }
+//   else if(marks>=50){
+//     console.log()
+//   }
+// }
+
+// let p2=new Promise((resolve,reject)=>{
+//   resolve(6)
+// })
+// p2.then((num)=>{
+// if(num%2==0){
+//   console.log("even")}
+//   else{
+//     console.log("odd")
+//   }
+
+// })
+
+
+// function add(a,b){
+// return a+b;
+// }
+// let res=add(5,8);
+// console.log(res);
+
+
+// function ask(a,b){
+//  return a*b;
+// }
+// let output=ask(4,16)
+// console.log(output)
+
+
+// let arr=[1,2,3,4,4,5,5];
+// console.log(arr.length);
+
+
+// let arr=[1,1,2,3,4,4];
+// console.log(arr[4]);
+
+// let arr =["a","b","c"];
+// // console.log(arr.includes("a"))
+
+
+// let name=[1,2,3,4,45,6];
+// name.push(4);
+// console.log(name);
+
+
+// let n=[1,2,3,4,5,5,6];
+// n.pop();
+// console.log(n);
+
+// let v=[2,3,4,5,5,3,2,1];
+// v.sort();
+// console.log(v);
+
+let t=[1,2,3,4,5,6,7];
+t.reverse();
+console.log(t);
